729b52ecfd7224b86d0cf0af99b863f93eefe7e5
chgang <gang.cheng1@fengjr.com>
2018-06-07 15:45:19 +0800

    springboot-advice rudiment

---
+++ springboot-advice/src/main/java/com/qskx/springbootadvice/SpringbootAdviceApplication.java
@@ -0,0 +1,12 @@
+package com.qskx.springbootadvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdviceApplication.class, args);
+	}
+}
---
+++ springboot-advice/src/main/java/com/qskx/springbootadvice/controller/MyControllerAdvice.java
@@ -0,0 +1,61 @@
+package com.qskx.springbootadvice.controller;
+
+import com.qskx.springbootadvice.exception.MyException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 111111
+ * @date 2018-06-07 15:22
+ */
+@ControllerAdvice
+public class MyControllerAdvice {
+
+    /**
+     * 应用到所有@RequestMapping注解方法，在其执行之前初始化数据绑定器
+     * @param binder
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {}
+
+    /**
+     * 把值绑定到Model中，使全局@RequestMapping可以获取到该值
+     * @param model
+     */
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("author", "Magical Sam");
+    }
+
+    /**
+     * 全局异常捕捉处理
+     * @param ex
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public Map errorHandler(Exception ex) {
+        Map map = new HashMap();
+        map.put("code", 100);
+        map.put("msg", ex.getMessage());
+        return map;
+    }
+
+    /**
+     * 拦截捕捉自定义异常 MyException.class
+     * @param ex
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(value = MyException.class)
+    public Map myErrorHandler(MyException ex) {
+        Map map = new HashMap();
+        map.put("code", ex.getCode());
+        map.put("msg", ex.getMsg());
+        return map;
+    }
+}
