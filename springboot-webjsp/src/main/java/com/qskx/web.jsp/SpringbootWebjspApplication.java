7df98340885656cea354cee64eb6d86516756d3b
chgang <chenggangsy@163.com>
2018-08-19 00:51:00 +0800

    springboot edit jsp

---
+++ springboot-webjsp/src/main/java/com/qskx/web.jsp/SpringbootWebjspApplication.java
@@ -0,0 +1,12 @@
+package com.qskx.web.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebjspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebjspApplication.class, args);
+	}
+}
