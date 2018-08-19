cb1fae17f329a27bd05d741cc46ce6218a0c9055
chgang <chenggangsy@163.com>
2018-08-19 22:16:58 +0800

    custom  Servletdispatcher

---
+++ springboot-servletdispatcher/src/test/java/com/qskx/servletdispatcher/SpringbootServletdispatcherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qskx.servletdispatcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootServletdispatcherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
