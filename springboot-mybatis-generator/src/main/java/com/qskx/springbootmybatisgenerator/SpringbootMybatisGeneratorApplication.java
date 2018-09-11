7ec1479ae62c51685ecfac051e9a19c6b1d15763
chgang <gang.cheng1@fengjr.com>
2018-09-11 09:44:21 +0800

    mybatis generator plug

---
+++ springboot-mybatis-generator/src/main/java/com/qskx/springbootmybatisgenerator/SpringbootMybatisGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.qskx.springbootmybatisgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisGeneratorApplication.class, args);
+    }
+}
