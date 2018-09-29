88cefd2b9356eddd3db2708731c0749e86a417dd
chgang <gang.cheng1@fengjr.com>
2018-09-29 18:52:34 +0800

    design mode

---
+++ springboot-design-mode/src/main/java/com/qskx/designmode/DesignModeApplication.java
@@ -0,0 +1,12 @@
+package com.qskx.designmode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModeApplication.class, args);
+    }
+}
