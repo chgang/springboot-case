1c36b52769bf5f6a9262a87bd75ee1134ba435bb
chgang <gang.cheng1@fengjr.com>
2018-10-09 14:05:28 +0800

    login class

--- springboot-quartz/src/main/java/com/qskx/quartz/controller/LoginController.java
+++ springboot-quartz/src/main/java/com/qskx/quartz/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.qskx.quartz.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @author 111111
  * @date 2018-09-15 21:39
  */
+@Controller
 public class LoginController {
 
 }
