666dea129603acadfbef81f5652c59b2d611edcc
chgang <chenggangsy@163.com>
2018-10-28 21:18:07 +0800

    master work

---
+++ springboot-sync/src/main/java/com/qskx/springbootsync/mastwork/Main.java
@@ -0,0 +1,20 @@
+package com.qskx.springbootsync.mastwork;
+
+/**
+ * @author 111111
+ * @date 2018-10-28 21:08
+ */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        FutureClient fc = new FutureClient();
+        Data data = fc.request("请求参数");
+        System.out.println("请求发送成功!");
+        System.out.println("做其他的事情...");
+
+        String result = data.getRequest();
+        System.out.println(result);
+
+    }
+}
