f20c64e373dfdad1acfc77e41add9fa3a57285df
chgang <chenggangsy@163.com>
2018-10-28 21:21:45 +0800

    java lock

---
+++ springboot-sync/src/main/java/com/qskx/springbootsync/lock/UseReentrantReadWriteLock.java
@@ -0,0 +1,79 @@
+package com.qskx.springbootsync.lock;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author 111111
+ * @date 2018-10-28 21:19
+ */
+public class UseReentrantReadWriteLock {
+
+    private ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private ReentrantReadWriteLock.ReadLock readLock = rwLock.readLock();
+    private ReentrantReadWriteLock.WriteLock writeLock = rwLock.writeLock();
+
+    public void read(){
+        try {
+            readLock.lock();
+            System.out.println("当前线程:" + Thread.currentThread().getName() + "进入...");
+            Thread.sleep(3000);
+            System.out.println("当前线程:" + Thread.currentThread().getName() + "退出...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public void write(){
+        try {
+            writeLock.lock();
+            System.out.println("当前线程:" + Thread.currentThread().getName() + "进入...");
+            Thread.sleep(3000);
+            System.out.println("当前线程:" + Thread.currentThread().getName() + "退出...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        final UseReentrantReadWriteLock urrw = new UseReentrantReadWriteLock();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                urrw.read();
+            }
+        }, "t1");
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                urrw.read();
+            }
+        }, "t2");
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                urrw.write();
+            }
+        }, "t3");
+        Thread t4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                urrw.write();
+            }
+        }, "t4");
+
+//		t1.start();
+//		t2.start();
+
+//		t1.start(); // R
+//		t3.start(); // W
+
+        t3.start();
+        t4.start();
+    }
+}
